c1f1299d4d87e2bb448e5615fcff3ee81678f9ec
Joshua H <josh@bedless.net>
2024-05-25 13:00:36 -0400

    Remove receivetest.py

--- receivetest.py
+++
@@ -1,9 +0,0 @@
-import socket
-address = '127.0.0.1'
-port = 8383
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((address, port))
-print(f"Listening for messages on {address}:{port}...")
-while True:
-    data, addr = sock.recvfrom(1024)
-    print(f"Received message from {addr}: {data.decode()}")
